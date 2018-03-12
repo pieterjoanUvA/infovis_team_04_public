fab98d74984f8b203d525ddbd682b96750ee0164
Pieter Joan van Voorst Vader <pieter.joan.vanvoorstvader@student.uva.nl>
2018-03-12 14:49:41 +0100

    added auto tour
    with automated transitions for in the presentation.

--- tour.js
+++ tour.js
@@ -41,6 +41,13 @@ $('#tour').click(function(e){
     // to avoid the click triggering whatever is within href:
     e.preventDefault();
 });
+$('#autotour').click(function(e){
+    // console.log("Been Clicked.")
+    autotour.restart();
+    // it's also good practice to preventDefault on the click event
+    // to avoid the click triggering whatever is within href:
+    e.preventDefault();
+});
 // Initialize the tour
 tour.init();
 
@@ -49,6 +56,42 @@ tour.start();
 // Restart the tour
 //tour.restart();
 //console.log('tourjs has been Executed')
+var autotour = new Tour({
+
+  backdrop: true,
+  backdropContainer: 'body',
+  backdropPadding: 0,
+  steps: [
+  {
+      duration: 4000,
+    placement: "right",
+    element: "#leftpanel",
+    title: "Leftpanel description",
+    content: "Here the donut chart shows differentiation between gender and age groups, below the deathcause is shown."
+  },
+  {
+      duration: 4000,
+    placement: "left",
+    element: "#rightpanel",
+    title: "Rightpanel description",
+    content: "News and Civilian / non-Civilian differentiation Donut chart."
+
+  },
+  {
+      duration: 4000,
+    placement: "top",
+    element: "#botrow",
+    title: "Bottom panel description",
+    content: "Linechart with totals."
+  },
+  {  duration: 4000,
+    placement: "right",
+    element: "#midpanel",
+    title: "Syria map/province description",
+    content: "Casualties per province in tooltips + color banding for global view."
+  }
+]});
+
 
 var tour_adv = new Tour({
   name: "tour",
